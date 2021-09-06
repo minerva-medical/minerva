93dc3fef72b39418cb41a17782123b04f5eaa0eb
Glen Larita <glarita@hawaii.edu>
2021-09-05 21:55:04 -1000

    Created Individual drug page
    Still need to incorporate collections and data.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -15,6 +15,7 @@ import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import DrugPage from '../pages/DrugPage';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -28,6 +29,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
+              <ProtectedRoute path="/drugpage" component={DrugPage}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
---
+++ app/imports/ui/pages/DrugPage.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Loader,
+  Item,
+  ItemDescription,
+  ItemContent,
+  ItemMeta,
+  ItemExtra,
+  Button, Segment, Grid, GridColumn, List, ListItem, ItemGroup,
+} from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Stuffs } from '../../api/stuff/StuffCollection';
+
+// import StuffItem from '../components/StuffItem';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class DrugPage extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the page once subscriptions have been received. */
+  renderPage() {
+    const drugBox = {
+      backgroundColor: 'lightblue',
+      borderRadius: '15px',
+      padding: '20px',
+      margin: '10px',
+    };
+    const notes = {
+      backgroundColor: 'white',
+      borderRadius: '15px',
+      padding: '20px',
+    };
+    return (
+        <Container>
+          <Header as="h2" textAlign="center">Drug Page</Header>
+          <Segment style={drugBox} size='medium'>
+            <Grid container divided columns='equal' stackable textAlign='justified'>
+              <GridColumn>
+                <ItemGroup relaxed>
+                  <Item>
+                    <ItemContent>
+                      <Header as='h2'> Aspirin 81 mg</Header>
+                      <ItemMeta>Nonsteroidal anti-inflammatory drug (NSAID)</ItemMeta>
+                      <ItemDescription>
+                        <List animated size='large'>
+                          <ListItem>Brand: Bayer</ListItem>
+                          <ListItem>Lot Number: 123456</ListItem>
+                          <ListItem>Expiration Date: 09/16/2021</ListItem>
+                          <ListItem>Quantity: 30 tabs</ListItem>
+                          <ListItem>Supply: 300</ListItem>
+                          <ListItem>Storage Location: Case 4</ListItem>
+                          <ListItem>Received: Purchased</ListItem>
+                        </List>
+                      </ItemDescription>
+                      <ItemExtra>
+                        <Button size='small'>Edit</Button>
+                      </ItemExtra>
+                    </ItemContent>
+                  </Item>
+                </ItemGroup>
+              </GridColumn>
+              <GridColumn>
+                <Segment style={notes}>
+                  <ItemGroup>
+                    <Item>
+                      <ItemContent>
+                        <Header as='h3'>Notes</Header>
+                        <ItemDescription>To be prescribed to patients that are at risk for myocardial infarction or
+                          stroke. Prescribed as take 1 tab daily.</ItemDescription>
+                      </ItemContent>
+                    </Item>
+                  </ItemGroup>
+                </Segment>
+              </GridColumn>
+            </Grid>
+          </Segment>
+        </Container>
+    );
+  }
+}
+
+/** Require an array of Stuff documents in the props. */
+DrugPage.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Stuffs.subscribeStuff();
+  return {
+    stuffs: Stuffs.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(DrugPage);
