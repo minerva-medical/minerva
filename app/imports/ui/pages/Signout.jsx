4cae42b29b137686cb44f6e3dc0482f4c547251e
alyssandra-cabading <alyssand@hawaii.edu>
2021-09-05 17:38:17 -1000

    SignOut Page Updated.

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header } from 'semantic-ui-react';
+import { Button, Container, Header, Message, Segment } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     Meteor.logout();
     return (
-      <Header as="h2" textAlign="center">
-        <p>You are signed out.</p>
-      </Header>
+       <div className='signOutPage' id='sign-out-page'>
+         <Segment style={{ padding: '8em 0em' }} vertical>
+           <Container text>
+             <i style={{ fontSize: '1.33em' }}>You have successfully signed out.</i>
+             <Header as='h3' style={{ fontSize: '9.5em' }}>MINERVA</Header>
+             <p style={{ textAlign: 'right', fontSize: '1.7em' }}><i>Come Back Soon!</i></p>
+             <Button.Group>
+               <Button as={NavLink} activeClassName="" exact to="/" key='landing' color="black" size='huge'>
+                 HOME
+               </Button>
+               <Button as={NavLink} activeClassName="" exact to="/signin" key='signin' color="white" size='huge'
+                       basic inverted>LOGIN</Button>
+             </Button.Group>
+           </Container>
+         </Segment>
+       </div>
     );
   }
 }
