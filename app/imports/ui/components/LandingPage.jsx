9080dd03fb974be1434eda2be5bf883ebd735b35
Glen Larita <glarita@hawaii.edu>
2021-09-09 13:10:02 -1000

    Fixed routing issue and page displaying incorrecly

--- app/imports/ui/components/LandingPage.jsx
+++ app/imports/ui/components/LandingPage.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Divider, Grid, Header, Button } from 'semantic-ui-react'
+import { Divider, Grid, Header, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 class LandingPage extends React.Component {
-    render() {
-        return (   
-            <div className="landing-center">
-                <Grid container centered>
-                    <Header className="landing-text" as="h1">MINERVA MEDICAL</Header>
-                </Grid>
-                <Divider section hidden/>
-                <Grid columns={2} verticalAlign="top" textAlign="center" container>
-                    <Grid.Column>
-                        <Button size="massive" as={NavLink} activeClassName="" exact to="/signup" key='signup' inverted style={{font:'Lato'}}>REGISTER</Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Button size="massive" as={NavLink} activeClassName="" exact to="/signin" key='signin' inverted style={{font:'Lato'}}>LOGIN</Button>
-                    </Grid.Column>
-                </Grid>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <div className="landing-center">
+          <Grid container centered>
+            <Header className="landing-text" as="h1">MINERVA MEDICAL</Header>
+          </Grid>
+          <Divider section hidden/>
+          <Grid columns={2} verticalAlign="top" textAlign="center" container>
+            <Grid.Column>
+              <Button size="massive" as={NavLink} activeClassName="" exact to="/signup" key='signup' inverted
+                      style={{ font: 'Lato' }}>REGISTER</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size="massive" as={NavLink} activeClassName="" exact to="/signin" key='signin' inverted
+                      style={{ font: 'Lato' }}>LOGIN</Button>
+            </Grid.Column>
+          </Grid>
+        </div>
+    );
+  }
 }
 
 export default LandingPage;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+// import { Grid, Image } from 'semantic-ui-react';
 import LandingPage from '../components/LandingPage';
 
 /** A simple static component to render some text for the landing page. */
