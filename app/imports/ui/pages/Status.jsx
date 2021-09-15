21b4172e16535550343ce6b54891b223b7b4b818
len-nguyen <70462461+len-nguyen@users.noreply.github.com>
2021-09-15 00:17:31 -1000

    Updated Status to reflect on reviews

--- app/imports/ui/pages/Status.jsx
+++ app/imports/ui/pages/Status.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Header, Form, Container, Loader, Table, Popup, Segment, Divider, Dropdown,
+import { Header, Form, Container, Loader, Table, Segment, Divider, Dropdown,
   Pagination, Icon, Grid } from 'semantic-ui-react';
 // import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { _ } from 'meteor/underscore';
 import { Sites } from '../../api/site/SiteCollection';
 import { Drugs } from '../../api/drug/DrugCollection';
 import { LotIds } from '../../api/lotId/LotIdCollection';
@@ -26,47 +24,44 @@ class Status extends React.Component {
       dispensedTo: undefined,
       dispensedFrom: undefined,
       inventoryType: undefined,
+      newSite: undefined,
     };
-    this.unitOptions = [
-      { key: '0', text: 'tabs', value: 'tabs' },
-      { key: '1', text: 'mL', value: 'mL' },
+    this.recordOptions = [
+      { key: '0', value: '10', text: '10' },
+      { key: '1', value: '25', text: '25' },
+      { key: '2', value: '50', text: '50' },
+      { key: '3', value: '100', text: '100' },
     ];
-    this.reasonOptions = [
-      { key: '0', text: '', value: '' },
-      { key: '1', text: 'Patient Use', value: 'Patient Use' },
-      { key: '2', text: 'Expired', value: 'Expired' },
-      { key: '3', text: 'Broken/Contaminated', value: 'Broken/Contaminated' },
-      { key: '4', text: 'Lost', value: 'Lost' },
+    this.medicationLocation = [
+      { key: '0', value: 'All', text: 'All' },
+      { key: '1', value: 'Case 1', text: 'Case 1' },
+      { key: '2', value: 'Case 2', text: 'Case 2' },
+      { key: '3', value: 'Case 3', text: 'Case 3' },
+      { key: '4', value: 'Case 4', text: 'Case 4' },
+      { key: '5', value: 'Case 5', text: 'Case 5' },
+      { key: '6', value: 'Case 6', text: 'Case 6' },
+      { key: '7', value: 'Case 7', text: 'Case 7' },
+      { key: '8', value: 'Case 8', text: 'Case 8' },
+      { key: '9', value: 'Refrigerator', text: 'Refrigerator' },
+      { key: '10', value: 'Freezer', text: 'Freezer' },
     ];
-  }
-
-  /** TODO: cleaner logic? */
-  componentDidUpdate() {
-    if (this.state.dispensedFrom === undefined && Meteor.user()) {
-      this.setState({ dispensedFrom: Meteor.user().username });
-    }
-  }
-
-  /** Update the form controls each time the user interacts with them. */
-  handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value });
-  }
-
-  /** On submit, insert the data. */
-  submit = event => {
-    event.preventDefault();
-
-    console.log('submitted');
-  }
-
-  /** convert array to dropdown options */
-  getOptions = name => {
-    const options = _.pluck(this.props[`${name}s`], name);
-    return options.map(elem => ({
-      key: elem,
-      text: elem,
-      value: elem,
-    }));
+    this.medicationType = [
+      { key: '0', value: 'All', text: 'All' },
+      { key: '1', value: 'Allergy & Cold Medicines', text: 'Allergy & Cold Medicines' },
+      { key: '2', value: 'Analgesics/Anti-inflammatory', text: 'Analgesics/Anti-inflammatory' },
+      { key: '3', value: 'Antimicrobials', text: 'Antimicrobials' },
+      { key: '4', value: 'Cardiac/Cholesterol', text: 'Cardiac/Cholesterol' },
+      { key: '5', value: 'Dermatologic Preparations', text: 'Dermatologic Preparations' },
+      { key: '6', value: 'Diabetes Meds', text: 'Diabetes Meds' },
+      { key: '7', value: 'Ear and Eye Preparations', text: 'Ear and Eye Preparations' },
+      { key: '8', value: 'Emergency Kit', text: 'Emergency Kit' },
+      { key: '9', value: 'GI Meds', text: 'GI Meds' },
+      { key: '10', value: 'GYN Meds', text: 'GYN Meds' },
+      { key: '11', value: 'Pulmonary', text: 'Pulmonary' },
+      { key: '12', value: 'Smoking Cessation', text: 'Smoking Cessation' },
+      { key: '13', value: 'Vitamins and Supplements', text: 'Vitamins and Supplements' },
+      { key: '14', value: 'Other', text: 'Other' },
+    ];
   }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
@@ -77,29 +72,8 @@ class Status extends React.Component {
   /** Render the form. */
   renderPage() {
 
-    const limitOptions = [
-      { key: '0', value: '10', text: '10' },
-      { key: '1', value: '25', text: '25' },
-      { key: '2', value: '50', text: '50' },
-      { key: '3', value: '100', text: '100' },
-    ];
-
-    const brand = [
-      { key: '0', value: 'All', text: 'All' },
-      { key: '1', value: 'Tylenol', text: 'Tylenol' },
-      { key: '2', value: 'Advil', text: 'Advil' },
-      { key: '3', value: 'Generic', text: 'Generic' },
-    ];
-
-    const type = [
-      { key: '0', value: 'All', text: 'All' },
-      { key: '1', value: 'Allergy & Cold Medicines', text: 'Allergy & Cold Medicines' },
-      { key: '2', value: 'Analgesics/Anti-inflammatory', text: 'Analgesics/Anti-inflammatory' },
-      { key: '3', value: 'Antihypertensives', text: 'Antihypertensives' },
-    ];
-
     return (
-        <Container id='inventory-status' text style={{ marginTop: '1em' }}>
+        <Container id='inventory-status' style={{ marginTop: '1em' }}>
           <Header as="h2">
             <Header.Content>
               Inventory Status
@@ -109,66 +83,49 @@ class Status extends React.Component {
               </Header.Subheader>
             </Header.Content>
           </Header>
-          <Segment text style={{ marginTop: '1em' }}>
+          <Segment style={{ marginTop: '1em' }}>
             <Grid>
-              <React.Fragment>
+              <div>
                 <Grid.Column width={8}>
                   <Form>
-                    <Form.Group>
-                      <Form.Field>
-                        <Popup
-                            trigger={
+                      <Form.Field style={{ marginBottom: '1em' }}>
                               <Form.Input floated={'right'}
                                           placeholder={'Enter a filter.'}
                                           name={'filter'}
                                           label={'Filter'}
                                           icon={'search'}
                               />
-                            }
-                            positon={'right center'}
-                        />
                       </Form.Field>
-                    </Form.Group>
                   </Form>
                 </Grid.Column>
                 <Grid.Column width={15}>
-                  Brand: {' '}
-                  <Dropdown
-                      inline={true}
-                      options={brand}
-                      defaultValue={'All'}
-                  />
                   Type: {' '}
                   <Dropdown
                       inline={true}
-                      options={type}
+                      options={this.medicationType}
+                      defaultValue={'All'}
+                  />
+                  Location: {' '}
+                  <Dropdown
+                      inline={true}
+                      options={this.medicationLocation}
                       defaultValue={'All'}
                   />
                 </Grid.Column>
-              </React.Fragment>
+              </div>
             </Grid>
             <Divider/>
-            <React.Fragment>
+            <div>
               Records per page:{' '}
               <Dropdown
                   inline={true}
-                  options={limitOptions}
-                  defaultValue={'25'}
+                  options={this.recordOptions}
+                  defaultValue={'10'}
               />
               Total count: {'200'}
               <Table celled selectable sortable>
                 <Table.Header>
                   <Table.Row>
-                    <Table.HeaderCell
-                        width={1}
-                    >
-                      #
-                    </Table.HeaderCell>
-                    <Table.HeaderCell
-                        width={3}
-                    >
-                      Brand
-                    </Table.HeaderCell>
                     <Table.HeaderCell
                         width={3}
                     >
@@ -180,9 +137,19 @@ class Status extends React.Component {
                       Medicine
                     </Table.HeaderCell>
                     <Table.HeaderCell
+                        width={3}
+                    >
+                      Location
+                    </Table.HeaderCell>
+                    <Table.HeaderCell
                         width={1}
                     >
                       Quantity
+                    </Table.HeaderCell>
+                    <Table.HeaderCell
+                        width={3}
+                    >
+                      LotId
                     </Table.HeaderCell>
                     <Table.HeaderCell
                         width={1}
@@ -191,31 +158,101 @@ class Status extends React.Component {
                     </Table.HeaderCell>
                   </Table.Row>
                   <Table.Row>
-                    <Table.Cell>1</Table.Cell>
-                    <Table.Cell>Tylenol</Table.Cell>
                     <Table.Cell>Analgesics/Anti-inflammatory</Table.Cell>
                     <Table.Cell>Acetaminophen 500 mg Caps</Table.Cell>
+                    <Table.Cell>Case 4</Table.Cell>
                     <Table.Cell>60</Table.Cell>
+                    <Table.Cell>12332A33</Table.Cell>
                     <Table.Cell textAlign='center'>
                       <Icon color='green' name='circle'/>
                     </Table.Cell>
                   </Table.Row>
                   <Table.Row>
-                    <Table.Cell>2</Table.Cell>
-                    <Table.Cell>Tylenol</Table.Cell>
                     <Table.Cell>Analgesics/Anti-inflammatory</Table.Cell>
                     <Table.Cell>Acetaminophen 160mg/5 ml Susp</Table.Cell>
+                    <Table.Cell>Case 4</Table.Cell>
                     <Table.Cell>2</Table.Cell>
+                    <Table.Cell>123AN413</Table.Cell>
                     <Table.Cell textAlign='center'>
                       <Icon color='yellow' name='circle'/>
                     </Table.Cell>
                   </Table.Row>
                   <Table.Row>
-                    <Table.Cell>3</Table.Cell>
-                    <Table.Cell>Tylenol</Table.Cell>
                     <Table.Cell>Analgesics/Anti-inflammatory</Table.Cell>
                     <Table.Cell>Acetaminophen Infant Drops</Table.Cell>
+                    <Table.Cell>Case 4</Table.Cell>
                     <Table.Cell>0</Table.Cell>
+                    <Table.Cell>6336AG46</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='red' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Pulmonary</Table.Cell>
+                    <Table.Cell>Steroid Inhaler</Table.Cell>
+                    <Table.Cell>Case 1</Table.Cell>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>A6326853</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='yellow' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Pulmonary</Table.Cell>
+                    <Table.Cell>Steroid/Long Acting Beta Agonist (Adult)</Table.Cell>
+                    <Table.Cell>Case 1</Table.Cell>
+                    <Table.Cell>3</Table.Cell>
+                    <Table.Cell>A0492792</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='green' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Diabetes Meds</Table.Cell>
+                    <Table.Cell>Glucose Tablets</Table.Cell>
+                    <Table.Cell>Case 8</Table.Cell>
+                    <Table.Cell>130</Table.Cell>
+                    <Table.Cell>A0498093</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='green' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Diabetes Meds</Table.Cell>
+                    <Table.Cell>DPP-4 Inhibitors (Sitagliptin 100 mg)</Table.Cell>
+                    <Table.Cell>Case 8</Table.Cell>
+                    <Table.Cell>100</Table.Cell>
+                    <Table.Cell>A0490626</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='green' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Diabetes Meds</Table.Cell>
+                    <Table.Cell>SGLT2 Inhibitors/DPP4 Inhibitor (10mg/5mg)</Table.Cell>
+                    <Table.Cell>Case 8</Table.Cell>
+                    <Table.Cell>58</Table.Cell>
+                    <Table.Cell>A0498627</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='green' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Allergy & Cold Medicines</Table.Cell>
+                    <Table.Cell>Diphenhydramine Soln</Table.Cell>
+                    <Table.Cell>Case 2</Table.Cell>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>A0495673</Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      <Icon color='red' name='circle'/>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Allergy & Cold Medicines</Table.Cell>
+                    <Table.Cell>Sore Throat Spray</Table.Cell>
+                    <Table.Cell>Case 6</Table.Cell>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>A0496798</Table.Cell>
                     <Table.Cell textAlign='center'>
                       <Icon color='red' name='circle'/>
                     </Table.Cell>
@@ -232,7 +269,7 @@ class Status extends React.Component {
                   </Table.Row>
                 </Table.Footer>
               </Table>
-            </React.Fragment>
+            </div>
           </Segment>
         </Container>
     );
