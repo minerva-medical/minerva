a68213d4b4155b564b32af3a51f0aaa6121f05e7
alyssandra-cabading <alyssand@hawaii.edu>
2021-09-15 19:16:34 -1000

    Update DispenseLog.jsx

--- app/imports/ui/pages/DispenseLog.jsx
+++ app/imports/ui/pages/DispenseLog.jsx
@@ -108,32 +108,33 @@ class DispenseLog extends React.Component {
           </Header>
           <Segment>
             <Grid>
-              <React.Fragment>
-                <Grid.Column width={15}>
-                  Sort: {' '}
+                <Grid.Row columns={5}>
+                  <Grid.Column>
+                  Sort By: {' '}
                   <Dropdown
                       inline={true}
                       options={sort}
                       defaultValue={'Most Recent'}
                   />
+                  </Grid.Column>
+                  <Grid.Column>
                   Reason for Dispense: {' '}
                   <Dropdown
                       inline={true}
                       options={reason}
                       defaultValue={'All'}
                   />
-                </Grid.Column>
-              </React.Fragment>
+                  </Grid.Column>
+                </Grid.Row>
             </Grid>
             <Divider/>
-            <React.Fragment>
               Records per page:{' '}
               <Dropdown
                   inline={true}
                   options={limitOptions}
-                  defaultValue={'25'}
+                  defaultValue={'10'}
               />
-              Total count: {'200'}
+            Total count: {'200'}
               <Table celled selectable sortable>
                 <Table.Header>
                   <Table.Row>
@@ -195,27 +196,59 @@ class DispenseLog extends React.Component {
                   <Table.Row>
                     <Table.Cell>2021-09-12 (07:45:20)</Table.Cell>
                     <Table.Cell>Expired</Table.Cell>
-                    <Table.Cell>Acetaminophen 500 mg Caps</Table.Cell>
-                    <Table.Cell>Advil</Table.Cell>
-                    <Table.Cell>60</Table.Cell>
-                    <Table.Cell>janedoe@hawaii.edu</Table.Cell>
+                    <Table.Cell>Cetirizine 10 mg tablets</Table.Cell>
+                    <Table.Cell>Zyrtic</Table.Cell>
+                    <Table.Cell>30</Table.Cell>
+                    <Table.Cell>sueflay@hawaii.edu</Table.Cell>
                   </Table.Row>
                   <Table.Row>
                     <Table.Cell>2021-09-11 (10:04:18)</Table.Cell>
                     <Table.Cell>Broken/Contaminated</Table.Cell>
-                    <Table.Cell>Acetaminophen 160mg/5 ml Susp</Table.Cell>
-                    <Table.Cell>Tylenol</Table.Cell>
-                    <Table.Cell>2</Table.Cell>
+                    <Table.Cell>Naproxen 500 mg tabs</Table.Cell>
+                    <Table.Cell>Aleve</Table.Cell>
+                    <Table.Cell>15</Table.Cell>
+                    <Table.Cell>obijuan@hawaii.edu</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>2021-09-10 (13:04:47)</Table.Cell>
+                    <Table.Cell>Patient Use</Table.Cell>
+                    <Table.Cell>Steroid Inhaler</Table.Cell>
+                    <Table.Cell>Ventolin</Table.Cell>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>maryjane@hawaii.edu</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>2021-09-10 (10:15:22)</Table.Cell>
+                    <Table.Cell>Patient Use</Table.Cell>
+                    <Table.Cell>Ibuprofen 800 mg tabs</Table.Cell>
+                    <Table.Cell>Advil</Table.Cell>
+                    <Table.Cell>60</Table.Cell>
                     <Table.Cell>johndoe@hawaii.edu</Table.Cell>
                   </Table.Row>
                   <Table.Row>
-                    <Table.Cell>2021-09-10 (13:04:67)</Table.Cell>
+                    <Table.Cell>2021-09-10 (09:35:17)</Table.Cell>
                     <Table.Cell>Patient Use</Table.Cell>
-                    <Table.Cell>Acetaminophen Infant Drops</Table.Cell>
-                    <Table.Cell>Advil</Table.Cell>
-                    <Table.Cell>2.5 mL</Table.Cell>
-                    <Table.Cell>doe@hawaii.edu</Table.Cell>
+                    <Table.Cell>Amoxicillin 250 mg Chewables</Table.Cell>
+                    <Table.Cell>Amoxil</Table.Cell>
+                    <Table.Cell>30</Table.Cell>
+                    <Table.Cell>janedoe@hawaii.edu</Table.Cell>
                   </Table.Row>
+                  <Table.Row>
+                  <Table.Cell>2021-09-10 (08:22:45)</Table.Cell>
+                  <Table.Cell>Expired</Table.Cell>
+                  <Table.Cell>Cold and Flu Syrup</Table.Cell>
+                  <Table.Cell>NyQuil</Table.Cell>
+                  <Table.Cell>355 mL</Table.Cell>
+                  <Table.Cell>sueflay@hawaii.edu</Table.Cell>
+                </Table.Row>
+                  <Table.Row>
+                  <Table.Cell>2021-09-09 (10:15:25)</Table.Cell>
+                  <Table.Cell>Expired</Table.Cell>
+                  <Table.Cell>Penicillin VK 500 mg Tabs</Table.Cell>
+                  <Table.Cell>Penicillin VK </Table.Cell>
+                  <Table.Cell>30</Table.Cell>
+                  <Table.Cell>obijuan@hawaii.edu</Table.Cell>
+                </Table.Row>
                 </Table.Header>
                 <Table.Footer>
                   <Table.Row>
@@ -228,7 +261,6 @@ class DispenseLog extends React.Component {
                   </Table.Row>
                 </Table.Footer>
               </Table>
-            </React.Fragment>
           </Segment>
         </Container>
     );
