0a8ada4ca23dee5647d95068eca73bc25c183e63
guanhongl <guanh.sl23@gmail.com>
2021-09-16 01:09:53 -1000

    fix review issues
    -converted to functional component

--- app/imports/ui/pages/Dispense.jsx
+++ app/imports/ui/pages/Dispense.jsx
@@ -1,4 +1,4 @@
-  import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Header, Form, Button, Container, Loader, Segment } from 'semantic-ui-react';
 // import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
@@ -10,187 +10,155 @@ import { Drugs } from '../../api/drug/DrugCollection';
 import { LotIds } from '../../api/lotId/LotIdCollection';
 import { Brands } from '../../api/brand/BrandCollection';
 
+/** convert array to dropdown options */
+const getOptions = (arr, name) => {
+  let options = _.pluck(arr, name);
+  options = options.map(elem => ({
+    key: elem,
+    text: elem,
+    value: elem,
+  }));
+  if (name === 'site') {
+    options.push({ key: 'OTHER', text: 'OTHER', value: 'OTHER' });
+  }
+  return options;
+};
+
+/** On submit, insert the data. */
+const submit = event => {
+  event.preventDefault();
+  // TODO: handle submit
+  console.log('submitted');
+};
+
 /** Renders the Page for Dispensing Inventory. */
-class Dispense extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      site: undefined,
-      dateDispensed: new Date().toISOString().slice(0, 10),
-      drug: undefined,
-      quantity: undefined,
-      brand: undefined,
-      lotId: undefined,
-      expire: undefined,
-      dispensedTo: undefined,
-      dispensedFrom: undefined,
-      inventoryType: undefined,
-      newSite: undefined,
-    };
-    this.unitOptions = [
-      { key: '0', text: 'tabs', value: 'tabs' },
-      { key: '1', text: 'mL', value: 'mL' },
-    ];
-    this.reasonOptions = [
-      { key: '1', text: 'Patient Use', value: 'Patient Use' },
-      { key: '2', text: 'Expired', value: 'Expired' },
-      { key: '3', text: 'Broken/Contaminated', value: 'Broken/Contaminated' },
-      { key: '4', text: 'Lost', value: 'Lost' },
-    ];
-    this.invenType = [
-      { key: '1', text: 'Medication', value: 'Medication' },
-      { key: '2', text: 'Vaccination', value: 'Vaccination' },
-      { key: '3', text: 'Patient Supplies', value: 'Patient Supplies' },
-      { key: '4', text: 'Lab Testing Supplies', value: 'Lab Testing Supplies' },
-    ];
-  }
+const Dispense = (props) => {
+  const [site, setSite] = useState('');
+  const [newSite, setNewSite] = useState('');
+  const [dateDispensed, setDateDispensed] = useState(new Date().toISOString().slice(0, 10));
+  const [drug, setDrug] = useState('');
+  const [quantity, setQuantity] = useState(NaN);
+  const [brand, setBrand] = useState('');
+  const [lotId, setLotId] = useState('');
+  const [expire, setExpire] = useState('');
+  const [dispensedTo, setDispensedTo] = useState('');
+  const [dispensedFrom, setDispensedFrom] = useState('');
 
-  /** TODO: cleaner logic? */
-  componentDidUpdate() {
-    if (this.state.dispensedFrom === undefined && Meteor.user()) {
-      this.setState({ dispensedFrom: Meteor.user().username });
-    }
-  }
-
-  /** Update the form controls each time the user interacts with them. */
-  handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value });
-  }
-
-  /** On submit, insert the data. */
-  submit = event => {
-    event.preventDefault();
-
-    console.log('submitted');
-  }
-
-  /** convert array to dropdown options */
-  getOptions = name => {
-    let options = _.pluck(this.props[`${name}s`], name);
-    options = options.map(elem => ({
-      key: elem,
-      text: elem,
-      value: elem,
-    }));
-    if (name === 'site') {
-      options.push({ key: 'OTHER', text: 'OTHER', value: 'OTHER' });
-    }
-    return options;
-  }
-
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-  render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  /** Render the form. */
-  renderPage() {
+  if (props.ready) {
     return (
-        <Container id='dispense-form' text style={{ marginTop: '1em' }}>
-          <Header as="h2">
-            <Header.Content>
-              Dispense from Inventory Form
-              <Header.Subheader>
-                <i>Please input the following information to dispense from the inventory,
-                  to the best of your abilities.</i>
-              </Header.Subheader>
-            </Header.Content>
-          </Header>
-          <Form onSubmit={this.submit}>
-            <Segment text style={{ marginTop: '1em' }}>
+      <Container id='dispense-form' text style={{ marginTop: '1em' }}>
+        <Header as="h2">
+          <Header.Content>
+            Dispense from Inventory Form
+            <Header.Subheader>
+              <i>Please input the following information to dispense from the inventory,
+                to the best of your abilities.</i>
+            </Header.Subheader>
+          </Header.Content>
+        </Header>
+        <Form onSubmit={submit}>
+          <Segment text style={{ marginTop: '1em' }}>
             {/* dispense info */}
-              <Grid columns='equal' >
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Input type="date" label='Date Dispensed' name='dateDispensed'
-                                onChange={this.handleChange} value={this.state.dateDispensed}/>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Form.Input label='Dispensed By' name='dispensedFrom'
-                                onChange={this.handleChange} value={this.state.dispensedFrom}/>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Select label='Inventory Type' name='inventoryType'
-                                 placeholder="Medication / Vaccination / Patient Supplies / Lab Testing Supplies"
-                                 onChange={this.handleChange} value={this.state.inventoryType}
-                                 options={this.invenType} clearable />
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Select label='Reason for Dispense' name='reasonDispense'
-                                 placeholder="Patient Use / Expired / Broken / Lost"
-                                 options={this.reasonOptions} clearable />
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Form.Input label='Dispensed To' placeholder="Patient's First Name, Last Name"
-                                name='dispensedTo' onChange={this.handleChange} value={this.state.dispensedTo}/>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Select clearable label='Site' options={this.getOptions('site')}
-                                 placeholder="POST, Kaka’ako, etc."
-                                 name='site' onChange={this.handleChange} value={this.state.site}/>
-                    {
-                      this.state.site === 'OTHER' &&
-                      <Form.Input name='newSite' onChange={this.handleChange} value={this.state.newSite}/>
-                    }
-                  </Grid.Column>
-                  {/* drug info */}
-                  <Grid.Column>
-                    <Form.Select clearable label='Drug Name' options={this.getOptions('drug')}
-                                 name='drug' onChange={this.handleChange} value={this.state.drug}/>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Form.Select clearable label='Lot Number' options={this.getOptions('lotId')}
-                                 name='lotId' onChange={this.handleChange} value={this.state.lotId}/>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Input type='date' label='Expiration Date'
-                                name='expire' onChange={this.handleChange} value={this.state.expire}/>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Form.Select clearable label='Brand' options={this.getOptions('brand')}
-                                 name='brand' onChange={this.handleChange} value={this.state.brand}/>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Form.Group>
-                      <Form.Input type='number' width={10} min={1} label='Quantity'
-                                  name='quantity' onChange={this.handleChange} value={this.state.quantity}/>
-                      <Form.Select label='Unit' className='unit-select'
-                                   options={this.unitOptions} width={4} fluid/>
-                    </Form.Group>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Form.Input label='Additional Notes'control='textarea' rows={2}
-                                onChange={this.handleChange} value={this.state.notes}>
-                    </Form.Input>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-            <div className='buttons-div'>
-              <Button type='submit' text style={{ marginTop: '1em' }}
-                      color="red" size='medium' inverted>Clear Fields</Button>
-              <Button type='submit' text style={{ marginTop: '1em' }}
-                      color="green" size='medium' floated="right" >Submit</Button>
-            </div>
-          </Form>
-        </Container>
+            <Grid columns='equal' >
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.Input type="date" label='Date Dispensed' name='dateDispensed'
+                              onChange={(e, { value }) => setDateDispensed(value)} value={dateDispensed}/>
+                </Grid.Column>
+                <Grid.Column>
+                  <Form.Input label='Dispensed By' name='dispensedFrom'
+                              onChange={(e, { value }) => setDispensedFrom(value)}
+                              value={dispensedFrom || props.currentUser.username}/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.Select label='Inventory Type' name='inventoryType'
+                               placeholder="Medication / Vaccination / Patient Supplies / Lab Testing Supplies"
+                               options={getOptions(props.types, 'type')} clearable />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.Select label='Reason for Dispense' name='reasonDispense'
+                               placeholder="Patient Use / Expired / Broken / Lost"
+                               options={getOptions(props.reasons, 'reason')} clearable />
+                </Grid.Column>
+                <Grid.Column>
+                  <Form.Input label='Dispensed To' placeholder="Patient's First Name, Last Name"
+                              name='dispensedTo' onChange={(e, { value }) => setDispensedTo(value)}
+                              value={dispensedTo}/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.Select clearable label='Site' options={getOptions(props.sites, 'site')}
+                               placeholder="POST, Kaka’ako, etc."
+                               name='site' onChange={(e, { value }) => setSite(value)} value={site}/>
+                  {
+                    site === 'OTHER' &&
+                    <Form.Input name='newSite' onChange={(e, { value }) => setNewSite(value)} value={newSite}/>
+                  }
+                </Grid.Column>
+                {/* drug info */}
+                <Grid.Column>
+                  <Form.Select clearable label='Drug Name' options={getOptions(props.drugs, 'drug')}
+                               name='drug' onChange={(e, { value }) => setDrug(value)} value={drug}/>
+                </Grid.Column>
+                <Grid.Column>
+                  <Form.Select clearable label='Lot Number' options={getOptions(props.lotIds, 'lotId')}
+                               name='lotId' onChange={(e, { value }) => setLotId(value)} value={lotId}/>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.Input type='date' label='Expiration Date'
+                              name='expire' onChange={(e, { value }) => setExpire(value)} value={expire}/>
+                </Grid.Column>
+                <Grid.Column>
+                  <Form.Select clearable label='Brand' options={getOptions(props.brands, 'brand')}
+                               name='brand' onChange={(e, { value }) => setBrand(value)} value={brand}/>
+                </Grid.Column>
+                <Grid.Column>
+                  <Form.Group>
+                    <Form.Input type='number' width={10} min={1} label='Quantity'
+                                name='quantity' onChange={(e, { value }) => setQuantity(value)} value={quantity}/>
+                    <Form.Select label='Unit' className='unit-select'
+                                 options={getOptions(props.units, 'unit')} width={4} fluid/>
+                  </Form.Group>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form.TextArea label='Additional Notes' />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+          <div className='buttons-div'>
+            <Button type='submit' text style={{ marginTop: '1em' }}
+                    color="red" size='medium' inverted>Clear Fields</Button>
+            <Button type='submit' text style={{ marginTop: '1em' }}
+                    color="green" size='medium' floated="right" >Submit</Button>
+          </div>
+        </Form>
+      </Container>
     );
   }
-}
+  return (<Loader active>Getting data</Loader>);
+};
 
 /** Require an array of Stuff documents in the props. */
 Dispense.propTypes = {
+  currentUser: PropTypes.object.isRequired,
+  sites: PropTypes.array.isRequired,
+  drugs: PropTypes.array.isRequired,
+  lotIds: PropTypes.array.isRequired,
+  brands: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
+  units: PropTypes.array.isRequired,
+  reasons: PropTypes.array.isRequired,
+  types: PropTypes.array.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
@@ -200,10 +168,16 @@ export default withTracker(() => {
   const lotIdSub = LotIds.subscribeLotId();
   const brandSub = Brands.subscribeBrand();
   return {
+    currentUser: Meteor.user(),
     sites: Sites.find({}).fetch(),
     drugs: Drugs.find({}).fetch(),
     lotIds: LotIds.find({}).fetch(),
     brands: Brands.find({}).fetch(),
     ready: siteSub.ready() && drugSub.ready() && lotIdSub.ready() && brandSub.ready(),
+    units: [{ unit: 'tabs' }, { unit: 'mL' }],
+    reasons: [{ reason: 'Patient Use' }, { reason: 'Expired' }, { reason: 'Broken/Contaminated' }, { reason: 'Lost' }],
+    // TODO: replace w/ tabs https://react.semantic-ui.com/modules/tab/
+    types: [{ type: 'Medication' }, { type: 'Vaccination' }, { type: 'Patient Supplies' },
+            { type: 'Lab Testing Supplies' }],
   };
 })(Dispense);
